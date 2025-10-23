e4f22aa4d3ce2a169967ccc4f8bf6f850a9dc1ae
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-10-23 14:34:26 -0400

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -74,7 +74,33 @@ class Character extends FlxSprite
 	public var alreadyLoaded:Bool = true; //Used by "Change Character" event
 
 	public static var DEFAULT_CHARACTER:String = 'bf'; //In case a character is missing, it will use BF on its place
-	public function new(x:Float, y:Float, ?character:String = 'bf', ?isPlayer:Bool = false)
+	
+	public var canAutoAnim:Bool = true;
+	public var canAutoIdle:Bool = true;
+
+	// 3D
+	public var modelView:ModelView;
+	public var beganLoading:Bool = false;
+	public var modelName:String = "";
+	public var modelScale:Float = 1;
+	public var model:ModelThing;
+	public var initYaw:Float = 0;
+	public var initPitch:Float = 0;
+	public var initRoll:Float = 0;
+	public var xOffset:Float = 0;
+	public var yOffset:Float = 0;
+	public var zOffset:Float = 0;
+	public var ambient:Float = 1;
+	public var specular:Float = 1;
+	public var diffuse:Float = 1;
+	public var animSpeed:Map<String, Float> = new Map<String, Float>();
+	public var noLoopList:Array<String> = [];
+	public var geoMap:Map<String, String> = new Map<String, String>();
+	public var atf:Bool = false;
+	public var light:Bool = false;
+	public var jointsPerVertex:Int = 4;
+
+	public function new(modelView:ModelView, x:Float, y:Float, ?character:String = 'bf', ?isPlayer:Bool = false)
 	{
 		super(x, y);
 
@@ -92,6 +118,90 @@ class Character extends FlxSprite
 		{
 			//case 'your character name in case you want to hardcode him instead':
 
+			case 'bf':
+				modelName = 'bf';
+				modelScale = 1;
+				animSpeed = ["default" => 1];
+				noLoopList = ["idle", "singUP", "singLEFT", "singRIGHT", "singDOWN"];
+				ambient = 0;
+				specular = 0;
+				diffuse = 1;
+				initYaw = 65;
+				zOffset = 150;
+				geoMap = [
+					"singUP" => "singUP",
+					"singRIGHT" => "singRIGHT",
+					"singDOWN" => "singDOWN",
+					"idle" => "default",
+					"idleEnd" => "default",
+					"singLEFT" => "singUP"
+				];
+				atf = true;
+			case 'gf':
+				modelName = 'gf';
+				modelScale = 1;
+				animSpeed = ["default" => 1];
+				noLoopList = ["danceLEFT", "danceRIGHT"];
+				ambient = 0;
+				specular = 0;
+				diffuse = 1;
+				xOffset = -100;
+				yOffset = -20;
+				atf = true;
+			case 'senpai':
+				modelName = 'senpai';
+				modelScale = 1;
+				animSpeed = ["default" => 1];
+				noLoopList = ["idle", "singUP", "singLEFT", "singRIGHT", "singDOWN"];
+				ambient = 0;
+				specular = 0;
+				diffuse = 1;
+				initYaw = -65;
+				zOffset = -150;
+				yOffset = 70;
+				geoMap = [
+					"singUP" => "singUP",
+					"singRIGHT" => "singRIGHT",
+					"singDOWN" => "singDOWN",
+					"singLEFT" => "singLEFT",
+					"idle" => "default",
+					"idleEnd" => "default"
+				];
+				antialias = false;
+			case 'senpai-angry':
+				modelName = 'senpai-angry';
+				modelScale = 1;
+				animSpeed = ["default" => 1];
+				noLoopList = ["idle", "singUP", "singLEFT", "singRIGHT", "singDOWN"];
+				ambient = 0;
+				specular = 0;
+				diffuse = 1;
+				initYaw = -65;
+				zOffset = -150;
+				yOffset = 70;
+				geoMap = [
+					"singUP" => "singUP",
+					"singRIGHT" => "singRIGHT",
+					"singDOWN" => "singDOWN",
+					"singLEFT" => "singLEFT",
+					"idle" => "default",
+					"idleEnd" => "default"
+				];
+				antialias = false;
+			case 'hydra':
+				modelName = 'hydra';
+				modelScale = 1;
+				animSpeed = ["default" => 1];
+				noLoopList = ["idle", "singUP", "singLEFT", "singRIGHT", "singDOWN"];
+				ambient = 0.5;
+				specular = 0.5;
+				diffuse = 1;
+				initYaw = 0;
+				xOffset = -150;
+				yOffset = 120;
+				atf = true;
+				light = true;
+				jointsPerVertex = 1;
 			default:
 				var characterPath:String = 'characters/' + curCharacter + '.json';
 				#if MODS_ALLOWED
@@ -169,6 +279,11 @@ class Character extends FlxSprite
 				}
 				//trace('Loaded file to character ' + curCharacter);
 		}
+
+		this.modelView = modelView;
+		model = new ModelThing(modelView, modelName, 'awd', animSpeed, noLoopList, modelScale, initYaw, initPitch, initRoll, xOffset, yOffset, zOffset, false,
+			antialias, atf, ambient, specular, light, jointsPerVertex);
+
 		originalFlipX = flipX;
 
 		recalculateDanceIdle();
@@ -202,6 +317,14 @@ class Character extends FlxSprite
 
 	override function update(elapsed:Float)
 	{
+		if (model == null || !model.fullyLoaded)
+			return;
+
+		if (model != null && model.fullyLoaded && modelView != null)
+		{
+			model.update();
+		}
+
 		if(!debugMode && animation.curAnim != null)
 		{
 			if(heyTimer > 0)
@@ -251,27 +374,121 @@ class Character extends FlxSprite
 	 */
 	public function dance()
 	{
-		if (!debugMode && !specialAnim)
-		{
-			if(danceIdle)
-			{
-				danced = !danced;
+		if (!model.fullyLoaded)
+			return;
 
-				if (danced)
-					playAnim('danceRight' + idleSuffix);
-				else
-					playAnim('danceLeft' + idleSuffix);
-			}
-			else if(animation.getByName('idle' + idleSuffix) != null) {
-					playAnim('idle' + idleSuffix);
+		if (!debugMode || ignoreDebug)
+		{
+			switch (curCharacter)
+			{
+				case 'gf' | 'gf-car' | 'gf-christmas' | 'gf-pixel':
+					if (!getCurAnim().startsWith('hair'))
+					{
+						danced = !danced;
+
+						if (danced)
+							playAnim('danceRIGHT', true);
+						else
+							playAnim('danceLEFT', true);
+					}
+				default:
+					if (holdTimer == 0)
+					{
+						if (model == null)
+						{
+							trace("NO DANCE - NO MODEL");
+							return;
+						}
+						if (!model.fullyLoaded)
+						{
+							trace("NO DANCE - NO FULLY LOAD");
+							return;
+						}
+						if (!noLoopList.contains('idle'))
+							return;
+						playAnim('idle', true);
+					}
 			}
 		}
+		else if (holdTimer == 0)
+		{
+			if (model == null)
+			{
+				trace("NO DANCE - NO MODEL");
+				return;
+			}
+			if (!model.fullyLoaded)
+			{
+				trace("NO DANCE - NO FULLY LOAD");
+				return;
+			}
+			if (!noLoopList.contains('idle'))
+				return;
+			playAnim('idle', true);
+		}
 	}
+
+	public function idleEnd(?ignoreDebug:Bool = false)
+	{
+		if (PlayState.instance.endingSong)
+			return;
+
+		if (!model.fullyLoaded)
+			return;
+
+		if ((!debugMode || ignoreDebug))
+		{
+			if (animExists(getCurAnim() + "End"))
+				playAnim(getCurAnim() + "End", true, false);
+			else if (animExists('idleEnd'))
+				playAnim('idleEnd', true, false);
+			else
+				playAnim('idle', true);
+		}
+	}
+
+	var curAtlasAnim:String;
 
 	public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void
 	{
 		specialAnim = false;
 		animation.play(AnimName, Force, Reversed, Frame);
+
+		if (!model.fullyLoaded)
+			return;
+
+		if (AnimName.endsWith('-alt') && !animExists(AnimName))
+		{
+			AnimName = AnimName.substring(0, AnimName.length - 4);
+		}
+
+		if (AnimName.contains('sing'))
+			canAutoIdle = true;
+
+		var geo:String = "";
+		if (geoMap[AnimName] != null)
+			geo = geoMap[AnimName];
+
+		if (AnimName.endsWith('miss'))
+		{
+			if (!animExists(AnimName))
+				AnimName = AnimName.substring(0, AnimName.length - 4);
+			geo = "miss";
+			model.modelMaterial.colorTransform.redMultiplier = 0.2;
+			model.modelMaterial.colorTransform.greenMultiplier = 0.2;
+			model.modelMaterial.colorTransform.blueMultiplier = 0.75;
+		}
+		else
+		{
+			model.modelMaterial.colorTransform.redMultiplier = 1;
+			model.modelMaterial.colorTransform.greenMultiplier = 1;
+			model.modelMaterial.colorTransform.blueMultiplier = 1;
+		}
+
+		if (model != null && model.fullyLoaded)
+		{
+			model.playAnim(AnimName, Force, Frame, geo);
+		}
 
 		var daOffset = animOffsets.get(AnimName);
 		if (animOffsets.exists(AnimName))
@@ -299,6 +516,22 @@ class Character extends FlxSprite
 		}
 	}
 
+	public function getCurAnim()
+	{
+		if (model != null && model.fullyLoaded)
+			return model.currentAnim;
+		else
+			return "";
+	}
+
+	public function animExists(anim:String)
+	{
+		if (model != null && model.fullyLoaded)
+			return model.animationSetSkeleton.hasAnimation(anim);
+		else
+			return false;
+	}
+
 	public function recalculateDanceIdle() {
 		danceIdle = (animation.getByName('danceLeft' + idleSuffix) != null && animation.getByName('danceRight' + idleSuffix) != null);
 	}
@@ -311,5 +544,19 @@ class Character extends FlxSprite
 	public function quickAnimAdd(name:String, anim:String)
 	{
 		animation.addByPrefix(name, anim, 24, false);
+	}
+
+	override public function destroy()
+	{
+		if (model != null)
+			model.destroy();
+		model = null;
+		modelView = null;
+		if (animSpeed != null)
+		{
+			animSpeed.clear();
+			animSpeed = null;
+		}
+		super.destroy();
 	}
 }
